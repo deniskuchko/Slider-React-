ca2842e09d0081afc7f5666e373ab50cf49eb86a
Denis_Kuchko <denis.kuchko@mail.ru>
2020-10-13 23:20:38 +0300

    Updates

---
+++ precache-manifest.8f5ea10216efa10be9378ec957a184d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051d6e79da25db611dd72d79cdad740e",
+    "url": "/Slider-React-/index.html"
+  },
+  {
+    "revision": "1e376f6b330dd36bd247",
+    "url": "/Slider-React-/static/css/main.d3d0bab8.chunk.css"
+  },
+  {
+    "revision": "49002d0f0bc92c60069b",
+    "url": "/Slider-React-/static/js/2.7d13dcac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Slider-React-/static/js/2.7d13dcac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e376f6b330dd36bd247",
+    "url": "/Slider-React-/static/js/main.b9e810e4.chunk.js"
+  },
+  {
+    "revision": "57792d548187ac026b45",
+    "url": "/Slider-React-/static/js/runtime-main.7a621e84.js"
+  },
+  {
+    "revision": "290174b6259ea557de7fde682836d3c6",
+    "url": "/Slider-React-/static/media/back.290174b6.svg"
+  },
+  {
+    "revision": "ff2b96719fb93f9698bfe4070dea4e27",
+    "url": "/Slider-React-/static/media/next.ff2b9671.svg"
+  }
+]);
